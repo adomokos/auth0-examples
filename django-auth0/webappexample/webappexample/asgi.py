1599f890bd616baeed45e9fe23a63c4d6addb7c6
Attila Domokos <adomokos@gmail.com>
2020-08-28 15:57:26 -0500

    Add the working Django example

--- django-auth0/webappexample/webappexample/asgi.py
+++ django-auth0/webappexample/webappexample/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for auth0django project.
+ASGI config for webappexample project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth0django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webappexample.settings')
 
 application = get_asgi_application()
