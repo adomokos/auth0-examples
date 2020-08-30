da2f9d2ebf271e716acbb292a1408537d6792805
Attila Domokos <adomokos@gmail.com>
2020-08-30 17:31:43 -0500

    Call the OIDC provider when server starts up

--- node-oidc-webapp/src/server.js
+++ node-oidc-webapp/src/server.js
@@ -58,6 +58,18 @@ app.get('/remove-to-do/:id', async (req, res) => {
   res.status(501).send();
 });
 
-app.listen(3000, () => {
-  console.log(`Server running on http://localhost:3000`);
-});
+const { OIDC_PROVIDER } = process.env;
+const discEnd = `https://${OIDC_PROVIDER}/.well-known/openid-configuration`;
+
+request(discEnd)
+  .then(res => {
+    oidcProviderInfo = JSON.parse(res);
+    app.listen(3000, () => {
+      console.log(`Server running on http://localhost:3000`);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+    console.error(`Unable to get OIDC endpoints for ${OIDC_PROVIDER}`);
+    process.exit(1);
+  });
